3d6ae9167eeb86854e31e26568173e3aeab472c2
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2020-10-08 15:31:16 +0300

    Updates

---
+++ precache-manifest.28d2fb27310c80bdbb5eb90e5c44ecb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d2834b6a1cf515c3ccc8c630db8c6a",
+    "url": "/react-image-finder/index.html"
+  },
+  {
+    "revision": "35529a0ca9a26e05fe7a",
+    "url": "/react-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "7afe4c001fc5853150e4",
+    "url": "/react-image-finder/static/css/main.720f05c1.chunk.css"
+  },
+  {
+    "revision": "35529a0ca9a26e05fe7a",
+    "url": "/react-image-finder/static/js/2.c58e0b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-finder/static/js/2.c58e0b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afe4c001fc5853150e4",
+    "url": "/react-image-finder/static/js/main.240f5c86.chunk.js"
+  },
+  {
+    "revision": "3f53ad26fd94709cddc1",
+    "url": "/react-image-finder/static/js/runtime-main.23981628.js"
+  }
+]);
